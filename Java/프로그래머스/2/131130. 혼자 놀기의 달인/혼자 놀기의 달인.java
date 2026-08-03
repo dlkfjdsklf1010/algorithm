30de402ddeeb4aeae151321e665f24d5d9732896
dlkfjdsklf1010 <dlkfjdsklf1010@gmail.com>
2026-08-03 09:37:58 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.27 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+
+    public int solution(int[] cards) {
+
+        boolean[] visited = new boolean[cards.length];
+        List<Integer> groups = new ArrayList<>();
+
+        for (int i = 0; i < cards.length; i++) {
+
+            if (visited[i]) continue;
+
+            int cnt = 0;
+            int cur = i;
+
+            while (!visited[cur]) {
+                visited[cur] = true;
+                cnt++;
+                cur = cards[cur] - 1;
+            }
+
+            groups.add(cnt);
+        }
+
+        if (groups.size() < 2) {
+            return 0;
+        }
+
+        Collections.sort(groups, Collections.reverseOrder());
+
+        return groups.get(0) * groups.get(1);
+    }
+}
