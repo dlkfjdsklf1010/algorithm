2b9b452ed8f24971b8133bae113691b58b09db3a
dlkfjdsklf1010 <dlkfjdsklf1010@gmail.com>
2026-08-10 23:53:37 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+       int total = 45;
+        
+        for(int num : numbers) {
+            total -= num;
+        }
+        
+        return total;
+    }
+}
