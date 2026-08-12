a891b58e6eaa3a22e1a7e45c10f0578d5fa6f40f
dlkfjdsklf1010 <dlkfjdsklf1010@gmail.com>
2026-08-12 09:55:15 +0900

    [level 3] Title: 섬 연결하기, Time: 0.69 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+
+        int[] parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int count = 0;
+
+        for (int[] cost : costs) {
+            int a = cost[0];
+            int b = cost[1];
+            int price = cost[2];
+
+            if (find(parent, a) != find(parent, b)) {
+                union(parent, a, b);
+
+                answer += price;
+                count++;
+                if (count == n - 1) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private int find(int[] parent, int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent, parent[x]);
+    }
+
+    private void union(int[] parent, int a, int b) {
+        int rootA = find(parent, a);
+        int rootB = find(parent, b);
+
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
+        }
+    }
+}
